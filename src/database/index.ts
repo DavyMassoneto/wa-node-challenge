fa33303f85901f4ca2663b97408dd90801d08a28
DavyMassoneto <davy.massoneto@yahoo.com>
2021-08-30 10:44:47 -0300

    fix: change the way the database settings are initialized

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,28 @@
-import { createConnection, getConnectionOptions } from 'typeorm'
+import { join } from 'path'
+import { ConnectionOptions, createConnection } from 'typeorm'
 
-interface IOptions {
-  host: string
+const baseDir = join(__dirname, '..')
+const entities = join(baseDir, '**', '**', '**', '*.js')
+const migrations = join(baseDir, '**', 'migrations', '*.js')
+
+function createTypeOrmOptions(): ConnectionOptions {
+  return {
+    type: 'postgres',
+    host: 'wa_challenge_postgresql',
+    database: process.env.TYPEORM_DATABASE,
+    port: Number(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    logging: process.env.TYPEORM_LOGGING === 'TRUE',
+    entities: [entities],
+    migrations: [migrations],
+    migrationsRun: process.env.TYPEORM_MIGRATIONS_RUN === 'TRUE',
+    cli: {
+      migrationsDir: migrations,
+      entitiesDir: entities,
+    },
+    synchronize: false,
+  }
 }
 
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions
-  newOptions.host = 'wa_challenge_postgresql'
-  createConnection(options)
-})
+createConnection(createTypeOrmOptions())
