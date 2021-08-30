28d86c2704d323462705fcb191e26063d45380d4
DavyMassoneto <davy.massoneto@yahoo.com>
2021-08-29 22:08:24 -0300

    feat: create migration for states

---
+++ src/database/migrations/1630284403033-create-states.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+import MigrationUtil from '../../utils/migrationUtil'
+
+export default class createStates1630284403033 implements MigrationInterface {
+  private table = new Table({
+    name: 'states',
+    columns: [
+      MigrationUtil.getIntegerColumn({ name: 'id', isPrimary: true }),
+      MigrationUtil.getVarCharColumn({ name: 'name' }),
+      MigrationUtil.getVarCharColumn({ name: 'abbreviation' }),
+      ...MigrationUtil.getDefaultColumns(),
+    ],
+  })
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table)
+  }
+}
