04e5832cf6ef82831f8ba87fc531ce7da85a9ab3
DavyMassoneto <davy.massoneto@yahoo.com>
2021-08-29 22:32:34 -0300

    feat: create laboratories migration

---
+++ src/database/migrations/1630286978425-create-laboratories.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+import MigrationUtil from '../../utils/migrationUtil'
+
+export default class createLaboratories1630286978425 implements MigrationInterface {
+  private table = new Table({
+    name: 'laboratories',
+    columns: [
+      MigrationUtil.getIntegerColumn({ name: 'id', isPrimary: true }),
+      MigrationUtil.getVarCharColumn({ name: 'name' }),
+      MigrationUtil.getVarCharColumn({ name: 'status' }),
+      MigrationUtil.getIntegerColumn({ name: 'city_id' }),
+      MigrationUtil.getVarCharColumn({ name: 'address' }),
+      ...MigrationUtil.getDefaultColumns(),
+    ],
+    foreignKeys: [MigrationUtil.getForeignKey({ referencedTableName: 'cities', columnName: 'city_id' })],
+  })
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table)
+  }
+}
