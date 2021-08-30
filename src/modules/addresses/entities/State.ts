88dd44e46e7c1e4c670735a23242a2da8e703fb3
DavyMassoneto <davy.massoneto@yahoo.com>
2021-08-30 01:06:28 -0300

    feat: add entity states

---
+++ src/modules/addresses/entities/State.ts
@@ -0,0 +1,21 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity('states')
+class State {
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  abbreviation: string
+
+  @CreateDateColumn({ name: 'createdAt' })
+  created_at: Date
+
+  @UpdateDateColumn({ name: 'updatedAt' })
+  updated_at: Date
+}
+
+export default State
